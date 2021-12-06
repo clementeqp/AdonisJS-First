c7b864efdd9882562157d067a8b633ded2f9ea3e
Clementeqp <clementeqp@hotmail.com>
2021-12-06 10:39:18 +0100

    fix candidates

--- database/migrations/1638462769727_experiences.ts
+++ database/migrations/1638462769727_experiences.ts
@@ -5,12 +5,12 @@ export default class Experiences extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments('id').primary()
 
       table.integer('candidate_id').references('candidates.id')
       table.integer('skill_id').references('id').inTable('skills')
 
-      table.enu('level', ['1', '2','3'])
+      table.integer('level')
 
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
