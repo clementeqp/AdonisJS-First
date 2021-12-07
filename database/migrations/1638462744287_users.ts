5416399e332311c4324393b3aa704447ff86782a
Clementeqp <clementeqp@hotmail.com>
2021-12-07 09:48:28 +0100

    unique mail en user

--- database/migrations/1638462744287_users.ts
+++ database/migrations/1638462744287_users.ts
@@ -7,7 +7,7 @@ export default class Users extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('name').notNullable()
-      table.string('email', 255).notNullable()
+      table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
